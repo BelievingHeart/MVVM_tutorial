820e27b19090a27b363e0cfe5cbd70c968d12384
Afterburner <rockey1024@icloud.com>
2019-08-28 09:40:48 +0800

    Using binding for visibility in place of DataTrigger

---
+++ ChromeWindowDemo/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using System.Globalization;
+using System.Windows;
+
+namespace ChromeWindowDemo
+{
+    public class BooleanToVisibilityConverter : ValueConverterBase<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var hasText = (bool) value;
+            return hasText ? Visibility.Hidden : Visibility.Visible;
+
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
